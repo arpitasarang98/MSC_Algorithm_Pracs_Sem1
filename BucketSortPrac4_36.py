d620d971cf54ca149c13f38bbbb9bc17398ef261
arpitasarang98 <81257878+arpitasarang98@users.noreply.github.com>
2021-03-23 23:53:03 +0530

    Add files via upload

---
+++ BucketSortPrac4_36.py
@@ -0,0 +1,7 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Iraje Software/Desktop/AlgorithmPracs/Bucket_sort_Algo_36.py
+Sorted Array is
+[0.1234, 0.3434, 0.565, 0.656, 0.665, 0.897]
+>>> 
